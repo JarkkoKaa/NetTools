885a082a429cbc165ea936e3e9d9caea46c40897
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2019-07-20 19:56:22 +0300

    Add project files.

---
+++ NetTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
