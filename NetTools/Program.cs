0a493668be9a99594fee72b39f2fb4552f48c5c0
JarkkoKaa <jarkko.kaartinen@edu.savonia.fi>
2019-07-20 21:32:20 +0300

    Init Ping

--- NetTools/Program.cs
+++ NetTools/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace NetTools
 {
@@ -6,7 +7,43 @@ namespace NetTools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte choice = 0;
+            bool isByte;
+
+            Console.WriteLine("\n+++++++++++++++\n" +
+                    "NET TOOLS\n" +
+                    "+++++++++++++++\n" +
+                    "Author: Jarkko Kaartinen - 2019\n\n" +
+                    $"Your machine: {Environment.MachineName}\n" +
+                    $"Your IP: {Dns.GetHostEntry(Dns.GetHostName())}\n");
+
+            while (choice != 99)
+            {
+                isByte = true;
+                
+                Console.WriteLine("1) Ping\n" +
+                    "99) Quit");
+                isByte = byte.TryParse(Console.ReadLine(), out choice);
+
+                if (!isByte)
+                    continue;
+
+                bool isCorrect;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Ping IP address: ");
+                        IPAddress ipAddress; 
+                        isCorrect = IPAddress.TryParse(Console.ReadLine(), out ipAddress);
+                        if (isCorrect)
+                        {
+                            SendPing sp = new SendPing(ipAddress);
+                        }
+                        else
+                            Console.WriteLine("Invalid address");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ NetTools/SendPing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace NetTools
+{
+    class SendPing
+    {
+        /// <summary>
+        /// Send ICMP echo request message to remote computer
+        /// </summary>
+        /// <param name="address"></param>
+        public SendPing(IPAddress address)
+        {
+            // synchronous ping
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions
+            {
+                DontFragment = true
+            };
+
+            // buffer or 32 bytes data to be transmitted
+            string data = "11111111111111111111111111111111";
+
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 1200;
+            PingReply reply = pingSender.Send(address, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("\n+++++++++++++++\n" +
+                    $"\nStatus: {reply.Status}\n" +
+                    $"Address: {reply.Address.ToString()}\n" +
+                    $"time: {reply.RoundtripTime}ms\n" +
+                    $"Time to live: {reply.Options.Ttl}\n" +
+                    $"Don't fragment: {reply.Options.DontFragment}\n" +
+                    $"Bytes: {reply.Buffer.Length}\n" +
+                    $"+++++++++++++++\n\n");
+            }
+        }
+    }
+}
